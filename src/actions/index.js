52267721cff71429d8f9c2459c8412a6e149dfcc
Pierre Lourens <plourens3@gmail.com>
2020-06-02 14:22:46 -0400

    Adds fetchPost actionCreator and action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const FETCH_POSTS = "fetch_posts";
+export const FETCH_POST = "fetch_post";
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=PAPERCLIP1234";
@@ -10,6 +11,15 @@ export function fetchPosts() {
 
   return {
     type: FETCH_POSTS,
+    payload: request,
+  };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
     payload: request,
   };
 }
