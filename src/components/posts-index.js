f70830283e17a82f3da0a0eb03394b3f1e8259b0
Pierre Lourens <plourens3@gmail.com>
2020-06-02 14:03:59 -0400

    Adds rendering of posts-list to posts-index.js

--- src/components/posts-index.js
+++ src/components/posts-index.js
@@ -1,6 +1,8 @@
+import _ from "lodash";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import { Link } from "react-router-dom";
 import { fetchPosts } from "../actions";
 
 class PostsIndex extends Component {
@@ -8,18 +10,37 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
-  render() {
-    console.log("The posts returned are ", this.props.posts);
-    return <div>List of blog posts will go here</div>;
+  renderPosts() {
+    return _.map(this.props.posts, (post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <Link to={`/posts/${post.id}`}>{post.title}</Link>
+        </li>
+      );
+    });
   }
+
+  render() {
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts };
 }
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ fetchPosts }, dispatch);
-}
-
-function mapStateToProps({ posts }) {
-  return { posts };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostsIndex);
