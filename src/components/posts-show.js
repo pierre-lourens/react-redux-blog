f0dbc24ad0ef6ad71741310badb74e28230b5898
Pierre Lourens <plourens3@gmail.com>
2020-06-02 14:45:02 -0400

    Adds fetching of posts to PostsShow

--- src/components/posts-show.js
+++ src/components/posts-show.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { fetchPost } from "../actions";
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
         <h1>Show a single post</h1>
@@ -11,4 +24,12 @@ class PostsShow extends Component {
   }
 }
 
-export default connect(null, null)(PostsShow);
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPost }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsShow);
